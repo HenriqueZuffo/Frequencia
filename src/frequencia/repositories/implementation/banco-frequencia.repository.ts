05b3896ee88d2905cf3662394a98326ef1e40fd4
Henrique Zuffo <henriquezuffo@gmail.com>
2023-03-22 19:01:37 -0300

    Ajuste no repo fake

--- src/frequencia/repositories/implementation/banco-frequencia.repository.ts
+++ src/frequencia/repositories/implementation/banco-frequencia.repository.ts
@@ -1,6 +1,16 @@
 import { CreateFrequenciaDTO } from 'src/frequencia/dtos/create-frequencia.dto';
+import { FrequenciaEntity } from 'src/frequencia/entities/frequencia.entity';
 import { FrequenciaRepository } from '../frequencia.repository';
 export class BancoFrequenciaRepository extends FrequenciaRepository{
+    getAll(): Promise<FrequenciaEntity[]> {
+        throw new Error('Method not implemented.');
+    }
+    getByCreatedAt(date: Date): Promise<FrequenciaEntity> {
+        throw new Error('Method not implemented.');
+    }
+    getByIdAtividade(id_atividade: string): Promise<FrequenciaEntity> {
+        throw new Error('Method not implemented.');
+    }
     create(frequenciaDTO: CreateFrequenciaDTO): Promise<void> {
         throw new Error('Method not implemented.');
     }
